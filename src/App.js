ebb669d86843a6decbf518f463b33a9737aa5335
Swadesh <swadesh2120@gmail.com>
2023-04-27 22:49:18 +0530

    fir

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Header/>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/techystar" element={<Home/>}/>
           <Route path="contact" element={<Contact/>}/>
           <Route path="/services" element={<Services/>}/>
         </Routes>
